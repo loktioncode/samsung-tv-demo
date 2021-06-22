b20a3a5890aea472884655c3fca0e66ba34e071d
DESKTOP-TEK06LE\EBere <elisha.b@cestasoft.co.za>
2021-06-22 11:14:38 +0200

    desktop version done

--- src/components/BacktoTop.js
+++ src/components/BacktoTop.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { Button } from 'react-bootstrap';
 
-export default ScrollArrow = () => {
+const icon = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 20 20">
+<path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+</svg>
+
+export default function ScrollArrow () {
   const [isVisible, setIsVisible] = useState(false);
 
 
@@ -25,11 +30,12 @@ export default ScrollArrow = () => {
   }, []);
 
   return (
-    <div className="scroll-to-top">
-      {isVisible && 
+    <div className="scrollTop">
+      {isVisible === true ? 
         <div onClick={scrollToTop}>
-          <img src='https://i.postimg.cc/44Ytsk8Z/top-arrow-emoj.png' alt='Go to top'/>
-        </div>}
+          <Button variant="success">{icon}</Button>{' '}
+        </div> : ''
+        }
     </div>
   );
 }
